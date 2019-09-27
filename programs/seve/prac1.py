c12859f708109a7479cefb6768f101b45e6d7cdf
severianovillarruel <severianovillarruel@gmail.com>
2019-09-27 15:11:01 -0700

    level up

--- programs/seve/prac1.py
+++ programs/seve/prac1.py
@@ -11,22 +11,6 @@ _ytrain_bound = []
 _xtrain_unbound = []
 _ytrain_unbound = []
 
-_xtrain=[]
-_ytrain=[]
-
-X_test=[]
-Y_test=[]
-
-count=0
-
-def count_yes(current_list):
-    current_count = 0
-    for i in range(len(current_list)):
-        if(current_list[i] <= -2):
-            current_count += 1
-    return current_count
-
-
 INPUT_FILE = open("/home/severiano/harms_proj/files/hA6.tsv", "r")
 
 for line in INPUT_FILE:
@@ -43,18 +27,35 @@ for line in INPUT_FILE:
         _ytrain_unbound.append(label)
 _xtrain_bound = np.array(_xtrain_bound)
 _xtrain_unbound = np.array(_xtrain_unbound)
-
-# print(len(_xtrain_bound))
+_ytrain_bound = np.array(_ytrain_bound)
+_ytrain_unbound = np.array(_ytrain_unbound)
 
 test_index=random.sample(range(len(_xtrain_bound)), (len(_xtrain_bound)//4))
 x_test_bound=_xtrain_bound[test_index]
 x_test_unbound=_xtrain_unbound[test_index]
+y_test_bound=_ytrain_bound[test_index]
+y_test_unbound=_ytrain_unbound[test_index]
 x_test=np.concatenate((x_test_bound,x_test_unbound), axis=0)
+y_test=np.concatenate((y_test_bound,y_test_unbound), axis=0)
 
-print(len(test_index))
+
 for i in test_index:
     x_train_bound=np.delete(_xtrain_bound,i,0)
     x_train_unbound=np.delete(_xtrain_unbound,i,0)
+    y_train_bound=np.delete(_ytrain_bound,i,0)
+    y_train_unbound=np.delete(_ytrain_unbound,i,0)
+x_train=np.concatenate((x_train_bound,x_train_unbound), axis=0)
+y_train=np.concatenate((y_train_bound,y_train_unbound), axis=0)
 
+def count_yes(current_list):
+    current_count = 0
+    for i in range(len(current_list)):
+        if(current_list[i] == 1):
+            current_count += 1
+    return current_count
 
+print("test YES:", count_yes(y_test))
+print("test NO:", len(y_test) - count_yes(y_test))
+print("train YES:", count_yes(y_train))
+print("train NO:", len(y_train) - count_yes(y_train))
 INPUT_FILE .close()
